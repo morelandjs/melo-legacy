5ab1381c43746d46eb02defb21d77f671d722dc9
Scott Moreland <jsm55@duke.edu>
2017-08-14 07:06:16 -0400

    !

--- elo.py
+++ elo.py
@@ -18,31 +18,15 @@ nweeks = 17
 nested_dict = lambda: defaultdict(nested_dict)
 
 class Rating:
-    def __init__(self, kfactor=50, decay=0.7, database='elo.db'):
+    def __init__(self, kfactor=40, decay=0.7, hfa=60.0, database='elo.db'):
         self.kfactor = kfactor
         self.decay = decay
+        self.hfa = hfa
 
         self.nfldb = nfldb.connect()
         self.spread_prob = self.spread_probability()
         self.elodb = nested_dict()
         self.calc_elo()
-
-    def hfa(self, handicap, year, week):
-        """
-        ELO bonus for home field advantage
-
-        """
-        hfa = []
-        for yr, wk in self.rewind(year, week, n=17):
-            home_rtg = self.query_elo('home', handicap, yr, wk)
-            away_rtg = self.query_elo('away', handicap, yr, wk)
-
-            hcap_diff = home_rtg['hcap'] - away_rtg['hcap']
-            fair_diff = home_rtg['fair'] - away_rtg['fair']
-
-            hfa.append(0.5*(hcap_diff + fair_diff))
-
-        return np.mean(hfa)
 
     def starting_elo(self, margin):
         """
@@ -81,20 +65,6 @@ class Rating:
 
         return dict(zip(spread, prob))
 
-    def ngames(self, year, week):
-        """
-        Number of games scheduled in a given (year, week)
-
-        """
-        # nfldb database
-        q = nfldb.Query(self.nfldb)
-        q.game(season_type='Regular', season_year=year, week=week)
-
-        # integer number of games
-        N = len(q.as_games())
-
-        return float(N)
-
     def rewind(self, year, week, n=2):
         """
         Simple function to go back "one game in time".
@@ -109,12 +79,6 @@ class Rating:
                 week = nweeks
             yield year, week
 
-    def regress(self, rtg, week):
-        if week == 1:
-            rtg['fair'] = 1500. + self.decay * (rtg['fair'] - 1500.)
-            rtg['hcap'] = 1500. + self.decay * (rtg['hcap'] - 1500.)
-        return rtg
-
     def query_elo(self, team, margin, year, week):
         """
         Queries the most recent ELO rating for a team, i.e.
@@ -124,22 +88,14 @@ class Rating:
         rating from the current year, week if it exists.
 
         """
-        def previous(year, week):
-            if team in ('home', 'away'):
-                yield year, week
-            for yr, wk in self.rewind(year, week):
-                yield yr, wk
-
-        for yr, wk in previous(year, week):
+        for yr, wk in self.rewind(year, week):
             elo = self.elodb[team][margin][yr][wk]
-            if elo:
-                return self.regress(elo.copy(), wk)
+            if elo: return elo.copy()
 
         elo = self.starting_elo(margin)
         self.elodb[team][margin][year-1][nweeks] = elo
 
-        return self.regress(elo, week)
-
+        return elo
 
     def elo_change(self, rating_diff, points, handicap):
         """
@@ -174,54 +130,45 @@ class Rating:
             year = game.season_year
             week = game.week
 
+            # team names
+            home = game.home_team
+            away = game.away_team
+
             # point differential
             points = game.home_score - game.away_score
 
-            # game team names
-            team_names = [
-                    ('home', 'away', self.ngames(year, week)),
-                    (game.home_team, game.away_team, 1.),
-                    ]
-            
-            # first update home/away ratings, then team ratings
-            for home, away, scale in team_names:
+            # loop over all possible spread margins
+            for handicap in range(41):
 
-                # loop over all possible spread margins
-                for handicap in range(41):
+                # query current elo ratings from most recent game
+                home_rtg = self.query_elo(home, handicap, year, week)
+                away_rtg = self.query_elo(away, handicap, year, week)
 
-                    # query current elo ratings from most recent game
-                    home_rtg = self.query_elo(home, handicap, year, week)
-                    away_rtg = self.query_elo(away, handicap, year, week)
-
-                    # disable hfa for home vs away elo calc
-                    hfa = (self.hfa(handicap, year, week)
-                            if scale == 1 else 0.)
-
-                    # elo change when home(away) team is handicapped
-                    bounty_home_hcap, bounty_away_hcap = [
-                            self.elo_change(
-                                home_rtg[a] - away_rtg[b] + hfa,
-                                points,
-                                hcap
-                                ) / scale
-                            for (a, b, hcap) in [
-                                ('hcap', 'fair', handicap),
-                                ('fair', 'hcap', -handicap),
-                                ]
+                # elo change when home(away) team is handicapped
+                bounty_home_hcap, bounty_away_hcap = [
+                        self.elo_change(
+                            home_rtg[a] - away_rtg[b] + self.hfa,
+                            points,
+                            hcap
+                            )
+                        for (a, b, hcap) in [
+                            ('hcap', 'fair', handicap),
+                            ('fair', 'hcap', -handicap),
                             ]
+                        ]
 
-                    # scale update by ngames if necessary
-                    home_rtg['hcap'] += bounty_home_hcap
-                    away_rtg['fair'] -= bounty_home_hcap
-                    home_rtg['fair'] += bounty_away_hcap
-                    away_rtg['hcap'] -= bounty_away_hcap
+                # scale update by ngames if necessary
+                home_rtg['hcap'] += bounty_home_hcap
+                away_rtg['fair'] -= bounty_home_hcap
+                home_rtg['fair'] += bounty_away_hcap
+                away_rtg['hcap'] -= bounty_away_hcap
 
-                    # home and away team elo data
-                    team_rtgs = [(home, home_rtg), (away, away_rtg)]
+                # home and away team elo data
+                team_rtgs = [(home, home_rtg), (away, away_rtg)]
 
-                    # update elo ratings
-                    for team, team_rtg in team_rtgs:
-                        self.elodb[team][handicap][year][week] = team_rtg
+                # update elo ratings
+                for team, team_rtg in team_rtgs:
+                    self.elodb[team][handicap][year][week] = team_rtg
 
     def cdf(self, home, away, year, week):
         """
@@ -236,19 +183,17 @@ class Rating:
             hcap = abs(handicap)
             home_rtg = self.query_elo(home, hcap, year, week)
             away_rtg = self.query_elo(away, hcap, year, week)
-            hfa = self.hfa(hcap, year, week)
 
             if handicap < 0:
-                rtg_diff = home_rtg['fair'] - away_rtg['hcap'] + hfa
+                rtg_diff = home_rtg['fair'] - away_rtg['hcap'] + self.hfa
             else:
-                rtg_diff = home_rtg['hcap'] - away_rtg['fair'] + hfa
+                rtg_diff = home_rtg['hcap'] - away_rtg['fair'] + self.hfa
 
             cumulative_win_prob.append(self.win_prob(rtg_diff))
 
         return spreads, gaussian_filter1d(
-                cumulative_win_prob, 2, mode='constant'
+                cumulative_win_prob, 0, mode='constant'
                 )
-
 
     def predict_spread(self, home, away, year, week):
         """
@@ -263,13 +208,12 @@ class Rating:
         median, prob = min(zip(*cdf), key=lambda k: abs(k[1] - 0.5))
 
         return median, prob
-
         
     def predict_score(self, home, away, year, week):
         # cumulative spread distribution
         spreads, cprob = self.cdf(home, away, year, week)
 
-        return sum(cprob) - 40.
+        return sum(cprob) - 40.5
 
     def win_prob(self, rtg_diff):
         """
@@ -279,7 +223,11 @@ class Rating:
         return 1./(10**(-rtg_diff/400.) + 1.)
 
     def model_accuracy(self):
-        # nfldb database
+        """
+        Calculate the mean and standard deviation of the model's
+        residuals = observed - predicted.
+
+        """
         q = nfldb.Query(self.nfldb)
         q.game(season_type='Regular')
 
@@ -293,17 +241,17 @@ class Rating:
             away = game.away_team
 
             # allow for one season burn-in
-            if year > 2012 and week > 8:
+            if year > 2011:
                 predicted = self.predict_score(home, away, year, week)
                 observed = game.home_score - game.away_score
                 residuals.append(observed - predicted)
 
-        return np.std(residuals)
+        return np.mean(residuals), np.std(residuals)
 
 def main():
     rating = Rating(kfactor=60, decay=0.6)
-    rms_error = rating.model_accuracy()
-    print(rms_error)
+    mean_error, rms_error = rating.model_accuracy()
+    print(mean_error, rms_error)
 
 if __name__ == "__main__":
     main()
