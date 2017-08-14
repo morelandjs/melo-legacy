50168e299792e904e45b6b2dd5a2d10e9a8f5cae
J. Scott Moreland <jsm55@duke.edu>
2017-08-14 07:40:15 -0400

    fix module

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='melo',
     version='0.1',
     description='NFL ELO rankings.',
-    url='https://github.com/nfl-elo.git',
+    url='https://github.com/melo.git',
     author='J. Scott Moreland',
     author_email='morelandjs@gmail.com',
-    packages=find_packages(exclude=('tests', 'docs')),
+    py_modules = ['melo']
     )
