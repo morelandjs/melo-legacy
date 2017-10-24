12091c8cb5add0dc5217be3ac2a684663c0d36c4
Scott Moreland <jsm55@duke.edu>
2017-10-24 18:37:53 -0400

    Some dict style cleanup

--- melo.py
+++ melo.py
@@ -5,17 +5,18 @@ from functools import total_ordering
 
 import numpy as np
 from scipy.optimize import minimize
+from skopt import gp_minimize
 
 import nfldb
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
 
 @total_ordering
 class Date:
     """
     Creates a cyclic date object with year and week
     attributes.
-    
+
     date.next and date.prev function calls increment
     and decrement the date object.
 
@@ -34,7 +35,7 @@ class Date:
     def __sub__(self, other):
         dy = (self.year - other.year)
         dw = (self.week - other.week)
-        return dy*self.nweeks + dw 
+        return dy*self.nweeks + dw
 
     @property
     def next(self):
@@ -56,29 +57,33 @@ class Rating:
     Rating class calculates margin-dependent Elo ratings.
 
     """
-    def __init__(self, obs='yards', mode='spread', kfactor=60, hfa=58,
-            decay=50, regress=0.7, database='elo.db'):
+    def __init__(self, obs='points', mode='spread', kfactor=59,
+                 decay=50, regress=0.64, database='elo.db'):
 
         # function to initialize a nested dictionary
-        nested_dict = lambda: defaultdict(nested_dict)
+        def nested_dict():
+            return defaultdict(nested_dict)
 
         # model hyper-parameters
         self.obs = obs
         self.mode = mode
         self.kfactor = kfactor
-        self.hfa = hfa
         self.decay = decay
         self.regress = regress
 
+        # home field advantage
+        self.hfa = {'spread': 56, 'total': 0}[mode]
+
         # point-spread interval attributes
-        self.bins= self.range(obs)
+        self.bins = self.bin_edges(obs)
         self.range = 0.5*(self.bins[:-1] + self.bins[1:])
 
         # list of team names
         self.teams = set()
 
+        # calculate Elo ratings
         self.nfldb = nfldb.connect()
-        self.last_game = self.last_played() 
+        self.last_game = self.last_played()
         self.spread_prob = self.spread_probability()
         self.elodb = nested_dict()
         self.calc_elo()
@@ -93,7 +98,6 @@ class Rating:
 
         last_game = {}
 
-        # look up most recent game
         for game in sorted(
                 q.as_games(),
                 key=lambda g: Date(g.season_year, g.week)
@@ -112,14 +116,20 @@ class Rating:
         rams.
 
         """
+        TINY = 1e-3
         elo_init = 1500.
-        prob = self.spread_prob[abs(margin)]
-        arg = np.clip(1./prob - 1., 1e-12, 1e12)
-        elo_diff = 200.*np.log10(arg)
 
-        # separate positive and negative ratings
-        # i.e. home team wins by 40 =  team[40] << team[-40]
-        return elo_init - elo_diff if margin > 0 else elo_init + elo_diff
+        margin = {'spread': margin, 'total': abs(margin)}[self.mode]
+        spread_prob = self.spread_prob[margin]
+        P = np.clip(spread_prob, TINY, 1 - TINY)
+
+        elo_diff = (
+                400*np.log10(1/P - 1)
+                if margin < 0 and self.mode == 'total' else
+                -400*np.log10(1/P - 1)
+                )
+
+        return elo_init + .5*elo_diff
 
     def spreads(self):
         """
@@ -129,7 +139,7 @@ class Rating:
         q = nfldb.Query(self.nfldb)
         q.game(season_type='Regular', finished=True)
         spreads = [self.point_diff(g) for g in q.as_games()]
-        
+
         return spreads
 
     def spread_probability(self):
@@ -137,11 +147,10 @@ class Rating:
         Probabilities of observing each point spread.
 
         """
-        spreads = self.spreads()
-        reversed_spreads = [-s for s in spreads]
-        hist, edges = np.histogram(
-                spreads + reversed_spreads, bins=self.bins, normed=True
-                )
+        ub = np.append(self.bins[1:], 1)
+        bins = {'spread': self.bins, 'total': self.bins[ub > 0]}[self.mode]
+
+        hist, edges = np.histogram(self.spreads(), bins=bins, normed=True)
         spread = 0.5*(edges[:-1] + edges[1:])
         bin_width = edges[1] - edges[0]
         prob = bin_width * np.cumsum(hist[::-1], dtype=float)[::-1]
@@ -158,6 +167,24 @@ class Rating:
 
         return default_rtg + factor * (rtg - default_rtg)
 
+    def adjust_injuries(self, team, year, week):
+        """
+        Add or subtract an injury bonus/penalty to the Elo rating
+
+        """
+        andrew_luck = (team == 'IND' and year == 2017 and week > 12)
+        carson_palmer = (team == 'ARI' and year == 2017 and 7 < week < 15)
+        aaron_rodgers = (team == 'GB' and year == 2017 and week > 6)
+
+        if andrew_luck:
+            return 100
+        elif aaron_rodgers:
+            return -100
+        elif carson_palmer:
+            return -75
+        else:
+            return 0
+
     def elo(self, team, margin, year, week):
         """
         Queries the most recent ELO rating for a team, i.e.
@@ -172,46 +199,48 @@ class Rating:
             last_year, last_week = (date_last.year, date_last.week)
             elo = self.elodb[team][margin][last_year][last_week]
 
-            elapsed = float(date - date_last)
+            # number of weeks since last game (don't count bye weeks)
+            elapsed = date - date_last - 1
             factor = np.exp(-elapsed/self.decay)
 
+            # adjust for injuries
+            elo += self.adjust_injuries(team, year, week)
+
+            # regress Elo to the mean
             return self.regress_to_mean(elo, margin, factor)
 
         # return the most recent Elo rating, account for bye weeks
         for d in date, date.prev:
             elo = self.elodb[team][margin][d.year][d.week]
-            if elo: return elo.copy()
+            if elo:
+                return elo.copy()
 
         return self.starting_elo(margin)
 
-    def yds(self, game, team):
+    def yards(self, game, team):
         """
         Calculates the yards traversed by a team over the course of a
         game.
 
         """
-        yards = 0.
+        def possession(drive):
+            return team == nfldb.standard_team(drive.pos_team)
 
-        for drive in filter(
-                lambda d: nfldb.standard_team(d.pos_team)==team,
-                game.drives
-                ):
-    
+        def progress(drive):
             try:
-                side, yardline = str(drive.end_field).split()
-                field = {'OPP': 100 - int(yardline), 'OWN': int(yardline)}
-                progress = field[side]
+                side, ydline = str(drive.end_field).split()
+                return {'OPP': 100 - int(ydline), 'OWN': int(ydline)}[side]
             except ValueError:
-                progress = 50
+                return 50
 
-            if drive.result == 'Touchdown':
-                yards += 100
-            elif drive.result == 'Field Goal':
-                yards += progress + (3/7)*(100 - progress)
-            else:
-                yards += progress
+        home, away = (game.home_team, game.away_team)
+        total = {home: game.home_score, away: game.away_score}[team]
 
-        return yards
+        for drive in filter(lambda d: possession(d), game.drives):
+            if drive.result not in ('Field Goal', 'Touchdown'):
+                total += 7*progress(drive)/100.
+
+        return total
 
     def point_diff(self, game):
         """
@@ -221,44 +250,34 @@ class Rating:
         """
         home, away = (game.home_team, game.away_team)
 
-        spread_dict = {
+        spread = {
                 "points": game.home_score - game.away_score,
-                "yards": self.yds(game, home) - self.yds(game, away)
-                }
+                "yards": self.yards(game, home) - self.yards(game, away),
+                }[self.obs]
 
-        total_dict = {
-                "points": game.home_score + game.away_score - 44,
-                "yards": self.yds(game, home) + self.yds(game, away)
-                }
+        total = {
+                "points": game.home_score + game.away_score,
+                "yards": self.yards(game, home) + self.yards(game, away),
+                }[self.obs]
 
-        mode_dict = {
-            "spread": spread_dict[self.obs],
-            "total": total_dict[self.obs]
-            }
+        return {"spread": spread, "total": total}[self.mode]
 
-        return mode_dict[self.mode]
-
-    def range(self, obs):
+    def bin_edges(self, obs):
         """
         Returns an iterator over the range of reasonable point values.
 
         """
-        spread_dict = {
+        spread = {
             "points": np.arange(-40.5, 41.5, 1),
-            "yards": np.arange(-495, 505, 10)
-            }
+            "yards": np.arange(-50.5, 51.5, 1),
+            }[self.obs]
 
-        total_dict = {
-            "points": np.arange(-45.5, 46.5, 1),
-            "yards": np.arange(-5, 1005, 10)
-            }
+        total = {
+            "points": np.arange(-100.5, 101.5, 1),
+            "yards": np.arange(-100.5, 101.5, 1),
+            }[self.obs]
 
-        mode_dict = {
-            "spread": spread_dict[obs],
-            "total": total_dict[obs]
-            }
-
-        return mode_dict[self.mode]
+        return {"spread": spread, "total": total}[self.mode]
 
     def elo_change(self, rating_diff, points, handicap):
         """
@@ -266,17 +285,17 @@ class Rating:
 
         """
         prob = self.win_prob(rating_diff)
+        win = self.kfactor * (1. - prob)
+        lose = -self.kfactor * prob
 
-        if points - handicap > 0:
-            return self.kfactor * (1. - prob)
-        else:
-            return -self.kfactor * prob
+        # TODO fix this for point totals
+        return win if points > handicap else lose
 
     def calc_elo(self):
         """
         This function calculates ELO ratings for every team
         for every value of the spread.
-        The rating reflects the posterior knowledge after the 
+        The rating reflects the posterior knowledge after the
         given week.
 
         """
@@ -285,8 +304,9 @@ class Rating:
         q.game(season_type='Regular', finished=True)
 
         # loop over historical games in chronological order
-        for game in sorted(q.as_games(),
-                key=lambda g: Date(g.season_year, g.week)):
+        for game in sorted(
+                q.as_games(), key=lambda g: Date(g.season_year, g.week)
+                ):
 
             # game date
             date = Date(game.season_year, game.week)
@@ -299,8 +319,14 @@ class Rating:
             # point differential
             points = self.point_diff(game)
 
+            # hcap range changes based on mode
+            hcap_range = {
+                    'spread': self.range,
+                    'total': self.range[self.range > 0],
+                    }[self.mode]
+
             # loop over all possible spread margins
-            for hcap in self.range:
+            for hcap in hcap_range:
 
                 # query current elo ratings from most recent game
                 home_rtg = self.elo(home, hcap, year, week)
@@ -318,9 +344,10 @@ class Rating:
                 next_year, next_week = (date.next.year, date.next.week)
                 updates = [(home, hcap, home_rtg), (away, -hcap, away_rtg)]
 
-                for (team, hcap, rtg) in updates: 
+                for (team, hcap, rtg) in updates:
                     if next_year > year:
                         rtg = self.regress_to_mean(rtg, hcap, self.regress)
+
                     self.elodb[team][hcap][next_year][next_week] = rtg
 
     def cdf(self, home, away, year, week):
@@ -331,15 +358,20 @@ class Rating:
         """
         cprob = []
 
-        for handicap in self.range:
-            home_rtg = self.elo(home, handicap, year, week)
-            away_rtg = self.elo(away, -handicap, year, week)
+        hcap_range = {
+                'spread': self.range,
+                'total': self.range[self.range > 0],
+                }[self.mode]
 
-            rtg_diff = home_rtg - away_rtg + self.hfa
+        for hcap in hcap_range:
+            home_rtg = self.elo(home, hcap, year, week)
+            away_rtg = self.elo(away, -hcap, year, week)
+            elo_diff = home_rtg - away_rtg + self.hfa
 
-            cprob.append(self.win_prob(rtg_diff))
+            win_prob = self.win_prob(elo_diff)
+            cprob.append(win_prob)
 
-        return self.range, cprob
+        return hcap_range, cprob
 
     def predict_spread(self, home, away, year, week, perc=0.5):
         """
@@ -351,7 +383,7 @@ class Rating:
 
         # plot median prediction (compare to vegas spread)
         index = np.square([p - perc for p in cprob]).argmin()
-        if index in (1, len(cprob) - 2): 
+        if index in range(1, len(cprob) - 2):
             x0, y0 = (spreads[index - 1], cprob[index - 1])
             x1, y1 = (spreads[index], cprob[index])
             x2, y2 = (spreads[index + 1], cprob[index + 1])
@@ -362,82 +394,106 @@ class Rating:
                     lambda x: np.square(np.polyval(coeff, x) - perc), x1
                   )
 
-            return res.x
+            return 0.5 * round(res.x * 2)
 
         return spreads[index]
 
     def predict_score(self, home, away, year, week):
         """
-        The model predicts the CDF of win margins, i.e. P(spread > x).
+        The model predicts the CDF of win margins, i.e. F = P(spread > x).
         One can use integration by parts to calculate the expected score
         from the CDF,
 
         E(x) = \int x P(x)
+             = x*F(x)| - \int F(x) dx
 
         """
         # cumulative spread distribution
-        spreads, cprob = self.cdf(home, away, year, week)
-        spread_max = max(self.range)
-        spread_step = spreads[1] - spreads[0]
+        x, F = self.cdf(home, away, year, week)
+        dx = (x[1] - x[0])
+        int_term = sum(F)*dx
+        x0, x1 = (min(x), max(x))
+        bdry_term = x1*F[-1] - x0*F[0]
 
-        # Calc via integration by parts of E(x) = \int x P(x)
-        return sum(cprob)*spread_step - spread_max - 0.5*spread_step
+        return int_term - bdry_term - .5*dx
 
     def win_prob(self, rtg_diff):
         """
-        Probability that a team will win as a function of ELO difference
+        Win probability as function of ELO difference
 
         """
         return 1./(10**(-rtg_diff/400.) + 1.)
 
-    def model_accuracy(self):
+    def model_accuracy(self, year=None):
         """
         Calculate the mean and standard deviation of the model's
         residuals = observed - predicted.
 
         """
         q = nfldb.Query(self.nfldb)
-        q.game(season_type='Regular', finished=True)
+        q.game(season_type='Regular', finished='True')
+        if year is not None:
+            q.game(season_year=year)
 
         residuals = []
 
-        # loop over all historical games
-        for game in sorted(q.as_games(),
-                key=lambda g: Date(g.season_year, g.week)):
+        for game in sorted(
+                q.as_games(),
+                key=lambda g: Date(g.season_year, g.week)
+                ):
+
             year = game.season_year
             week = game.week
             home = game.home_team
             away = game.away_team
 
-            # allow for two season burn-in
-            if year > 2010:
+            # allow for one season burn-in
+            if year > 2009:
                 predicted = self.predict_score(home, away, year, week)
-                observed = self.point_diff(game)
+                observed = game.home_score - game.away_score
                 residuals.append(observed - predicted)
 
         return residuals
+
+    def optimize(self):
+        """
+        Function to optimize model hyper-parameters
+
+        """
+        def objective(parameters):
+            """
+            Evaluates the mean absolute error for a set of input
+            parameters: kfactor, decay, regress.
+
+            """
+            kfactor, decay, regress = parameters
+            rating = Rating(kfactor=kfactor, decay=decay, regress=regress)
+            residuals = rating.model_accuracy()
+            mean_abs_error = np.abs(residuals).mean()
+            return mean_abs_error
+
+        bounds = [(10, 100), (10, 10**3, "log-uniform"), (0.1, 1)]
+        res_gp = gp_minimize(objective, bounds, n_calls=10, random_state=0)
+
+        print("Best score: {:.4f}".format(res_gp.fun))
+        print("Best parameters: {}".format(res_gp.x))
+
 
 def main():
     """
     Main function prints the model accuracy parameters and exits
 
     """
-    rating = Rating()
+    rating = Rating(database='elo.db')
     residuals = rating.model_accuracy()
     mean_error = np.mean(residuals)
     rms_error = np.std(residuals)
-    print "residual mean:", mean_error
-    print "residual std:", rms_error
+    mean_abs_error = np.abs(residuals).mean()
 
-    rank = {}
+    print("residual mean:", mean_error)
+    print("residual std:", rms_error)
+    print("residual abs error:", mean_abs_error)
 
-    for team in rating.teams:
-        date = rating.last_game[team].next
-        elo = rating.elo(team, 0, date.year, date.week)
-        rank.update({team: elo})
-
-    for team in sorted(rank, key=rank.get, reverse=True):
-        print team, int(rank[team])
 
 if __name__ == "__main__":
     main()
